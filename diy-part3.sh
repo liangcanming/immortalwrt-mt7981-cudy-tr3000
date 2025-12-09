07622a9585af1ecb94cba7bacfdc1d4931e8e336
liangcanming <410757076@qq.com>
2025-12-09 09:45:45 +0800

    Add files via upload

---
+++ diy-part3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# Enable USB power for Cudy TR3000 by default
+sed -i '/modem-power/,/};/{s/gpio-export,output = <1>;/gpio-export,output = <0>;/}' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1.dtsi
+
+# add date in output file name
+sed -i -e '/^IMG_PREFIX:=/i BUILD_DATE := $(shell date +%Y%m%d)' \
+       -e '/^IMG_PREFIX:=/ s/\($(SUBTARGET)\)/\1-$(BUILD_DATE)/' include/image.mk
+
+# set ubi to 122M
+# sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
+
+# set ubi to 512M
+sed -i 's/model = "Cudy TR3000 v1 ubi 112M";/model = "Cudy TR3000 v1 512MB";/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
+sed -i 's/model = compatible = "cudy,tr3000-v1-ubootmod", "mediatek,mt7981";/compatible = "cudy,tr3000-v1-512mb", "mediatek,mt7981";/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
+sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x1ea0000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
+
+# Add OpenClash Meta
+mkdir -p files/etc/openclash/core
+
+wget -qO "clash_meta.tar.gz" "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+tar -zxvf "clash_meta.tar.gz" -C files/etc/openclash/core/
+mv files/etc/openclash/core/clash files/etc/openclash/core/clash_meta
+chmod +x files/etc/openclash/core/clash_meta
+rm -f "clash_meta.tar.gz"
